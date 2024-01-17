acbd20f170ea9ab0cf5464e1063ce20596e5b87e
dhommen <dh@orbiter.de>
2024-01-17 20:22:13 +0100

    missing "

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ subprojects {
             maven {
                 name = "GitHubPackages"
                 url = uri("https://maven.pkg.github.com/truzzt/mds-ap3")
-                version = "0.1.0
+                version = "0.1.0"
                 credentials {
                     username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                     password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
