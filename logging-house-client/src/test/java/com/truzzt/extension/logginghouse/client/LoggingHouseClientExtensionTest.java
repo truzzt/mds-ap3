3eb42ebf23d763e141a06dbdd08d706d5e8d963d
dhommen <dh@orbiter.de>
2024-01-25 15:32:59 +0100

    checkstyle ...

--- logging-house-client/src/test/java/com/truzzt/extension/logginghouse/client/LoggingHouseClientExtensionTest.java
+++ logging-house-client/src/test/java/com/truzzt/extension/logginghouse/client/LoggingHouseClientExtensionTest.java
@@ -1,11 +1,11 @@
-import com.truzzt.extension.logginghouse.client.LoggingHouseClientExtension;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.eclipse.edc.spi.system.ServiceExtensionContext;
+
+import com.truzzt.extension.logginghouse.client.LoggingHouseClientExtension;
 
 class LoggingHouseClientExtensionTest {
 
