810d7c173f9d10da8ea6cb23f43d894588c16289
Maximilian Schönenberg <schoenenberg@users.noreply.github.com>
2024-07-09 19:03:11 +0200

    fix: ordering of pending statements

--- logging-house-client/src/main/java/com/truzzt/extension/logginghouse/client/store/sql/schema/BaseSqlDialectStatements.java
+++ logging-house-client/src/main/java/com/truzzt/extension/logginghouse/client/store/sql/schema/BaseSqlDialectStatements.java
@@ -37,9 +37,11 @@ public class BaseSqlDialectStatements implements LoggingHouseEventStatements {
 
     @Override
     public String getSelectPendingStatement() {
-        return format("SELECT * FROM %s WHERE %s IS NULL",
+        return format("SELECT * FROM %s WHERE %s IS NULL ORDER BY %s asc, %s asc",
                 getLoggingHouseMessageTable(),
-                getReceiptColumn());
+                getReceiptColumn(),
+                getCreatedAtColumn(),
+                getIdColumn());
     }
 
     @Override
